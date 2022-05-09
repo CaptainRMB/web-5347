48904f510d7fb33ba290da02f82361bdaa8dbd85
BOJIA <bzha7778@uni.sydney.edu.au>
2022-05-09 17:19:54 +1000

    Update userController.js

--- controller/userController.js
+++ controller/userController.js
@@ -4,7 +4,11 @@ const _url = require('url');
 const _util = require('../utils/hashing')
 const _ws = require('../websocketServer')
 const _webSocket = require("ws");
+const nodemailer  = require('nodemailer');
 
+const {isEmail}= require('../utils/validator');
+const { isPromise } = require("util/types");
+const { isPwdValidated } = require("../utils/validator");
 module.exports = {
 
     async userLogin(req, res) {
@@ -64,23 +68,62 @@ module.exports = {
 
     async userSignUp(req, res) {
         let {firstName, lastName, email, password} = req.body
+        
+        var permission=isPwdValidated(password);
+        var mail = isEmail(email);
         password = _util.md5(password);
-        let result = await _usersQuery.signUp(email, password, firstName, lastName);
-        if (result === true) {
-            res.send("Sign Up Successfully!")
+        if (permission===true&&mail===true) {
+        if (await _usersQuery.signUp(email, password, firstName, lastName) === true) {
+            res.send("Sign Up Successfully!")}
             // await setTimeout( function(){
             //     res.redirect(302, '/login.html');
             // }, 2 * 1000 );
         }
         else {
             res.status(403)
-            if (result.code === 11000) {
-                res.send("Sign Up failed: " + result.name + result.message)
-            }
-            else {//TODO case if no error code is returned
-                res.send("Sign Up failed: " + result.name + result.message)
-            }
+            if(await _usersQuery.signUp(email, password, firstName, lastName).code !== 11000)
+            res.send("Sign Up failed: " + "THE PASSWORD OR EMAIL_IS_INVALID");
+            else
+            res.send("Sign Up failed: " + await _usersQuery.signUp(email, password, firstName, lastName).name + await _usersQuery.signUp(email, password, firstName, lastName).message);
+            // if (result.code === 11000) {
+            //     res.send("Sign Up failed: " + result.name + result.message)
+            // }
+            // else {//TODO case if no error code is returned
+            //     res.send("Sign Up failed: " + result.name + result.message)
+            // }
         }
+
+        const mailTransport = nodemailer.createTransport({
+            host : 'smtp.163.com',
+            port: 465, // SMTP 
+            secureConnection: true, // SSL
+            auth : {
+                user : 'zbj2305@163.com',
+                pass : 'SBCPIUYZMOCRJCDO'
+            },
+        });
+
+
+
+        var options = {
+            from        : '"Phone market" zbj2305@163.com',
+            to          : email,
+            subject        : 'A email from Node Mailer',
+            text          : 'A email from Node Mailer',
+            html           : '<h1>Hey，this email is from Node Mailer！</h1>',
+        };
+
+
+        mailTransport.sendMail(options, function(err, msg){
+            if(err){
+                console.log(err);
+                res.render('index', { title: err });
+            }
+            else {
+                console.log(msg);
+                res.render('index', { title: "received："+msg.accepted});
+            }
+        });
     },
 
     async signOut(req, res) {
